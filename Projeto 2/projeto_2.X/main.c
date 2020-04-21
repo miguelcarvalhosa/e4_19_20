b0abf4758a708d03397c5ede3f9868b9cb70befa
Miguel Carvalhosa <miguelcarvalhosa@ua.pt>
2020-04-21 09:33:21 +0100

    Add projeto_2.x

---
+++ Projeto 2/projeto_2.X/main.c
@@ -0,0 +1,42 @@
+
+#include <stdint.h>
+#include <xc.h>
+
+#include "../../Sources/config_bits.h"
+#include "../../Sources/timer2.h"
+#include "../../Sources/timer3.h"
+#include "../../Sources/adc.h"
+#include "../../Sources/uart.h"
+#include "../../Sources/delay.h"
+
+
+/*
+ * Definições
+ */
+#define SYSCLK			80000000
+#define PBCLK			SYSCLK/2
+#define PWM_STEPS		256			// Se alterado, deve-se alterar também no ficheiro "timer3.h"
+
+
+/*
+ * Protótipos das funções
+ */
+
+
+
+/*
+ * Main
+ */
+int main(int argc, char** argv) {
+	
+	
+	
+	while(1) {
+		
+	}
+	
+	
+	return (EXIT_SUCCESS);
+}
+
+
